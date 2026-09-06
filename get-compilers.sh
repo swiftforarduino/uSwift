71b45e37f600fadc01d9f15c8a57bf913569a6e7
Carl Peto <carl.peto@me.com>
2026-09-06 03:38:05 +0100

    attempt to keep copies of compiler if it has not changed

--- get-compilers.sh
+++ get-compilers.sh
@@ -11,7 +11,7 @@ then
 fi
 
 function cleanup_staging {
-  rm -rf $STAGING 2> /dev/null
+  rm -rf "$STAGING" 2> /dev/null
 }
 
 trap cleanup_staging EXIT
@@ -20,26 +20,48 @@ function get_compiler_zip {
 
   echo "Downloading ${1}..."
 
-  pushd ${STAGING}
+  pushd "$STAGING"
   rm -rf * 2> /dev/null
 
   REQUEST="requested_filename=${1}.zip"
-  if (curl -Ljb cookies.txt "${COMPILERS}${REQUEST}" > ${1}.zip) && (unzip ${1}.zip)
+  LOCAL_ZIP="${2}/${1}.zip"
+
+  if [ -f "$LOCAL_ZIP" ]
   then
-    popd
-    rm -rf ${2} 2> /dev/null
-    mkdir -p ${2}
-    cp -a ${STAGING}/${1}/* ${2}/
+    echo "  Local copy exists; checking whether it has changed..."
+    CURL_TIME="-z ${LOCAL_ZIP}"
+  else
+    CURL_TIME=""
+  fi
+
+  if curl -Ljb cookies.txt $CURL_TIME "${COMPILERS}${REQUEST}" -o "${1}.zip"
+  then
+    if [ -s "${1}.zip" ] && unzip "${1}.zip"
+    then
+      popd
+      rm -rf "$2" 2> /dev/null
+      mkdir -p "$2"
+      cp -a "${STAGING}/${1}"/* "$2"/
+    elif [ -f "$LOCAL_ZIP" ]
+    then
+      echo "  Server says local copy is up to date."
+      popd
+    else
+      echo "  Download failed."
+      popd
+      exit 99
+    fi
   else
     popd
     exit 99
   fi
 }
 
-if get_compiler_zip swift-6.5 $1
+if get_compiler_zip swift-6.5 "$1"
 then
   echo retrieving swift 6.5 compiler complete
 else
   echo retrieving swift 6.5 compiler failed
   exit 1
 fi
+
